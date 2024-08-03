07576238ff732d441029b8d3c30aa6a0cea5f055
saidAbdirahman <saidqaaje11@gmail.com>
2024-08-03 20:01:30 +0200

    Setuped laravel and vue
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    protected $student;
+    public function __construct(){
+        $this->student = new Student();
+        
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return $this->student->all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return $this->student->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+       $student = $this->student->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $student = $this->student->find($id);
+        $student->update($request->all());
+        return $student;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $student = $this->student->find($id);
+    }
+}
